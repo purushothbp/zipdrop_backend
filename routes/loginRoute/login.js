e280fd08dd0bdf3655a7da9b5a89732f13ec5b14
purushothbp <bpurushothamandasu@gmail.com>
2024-01-21 08:14:11 +0530

    resend-otp

--- routes/loginRoute/login.js
+++ routes/loginRoute/login.js
@@ -13,39 +13,94 @@ const accessToken = config.ACCESS_TOKEN;
 
 // Endpoint to generate OTP as WhatsApp message
 app.post('/sendmessage', async (req, res) => {
-    try {
-      const { whatsappNumber } = req.body;
-  
-      const MAX_DIGITS = 12;
+  try {
+    const { whatsappNumber } = req.body;
 
-      if (!whatsappNumber) {
-        return res.status(400).json({ success: false, message:strings.WhatsappNumberRequired});
-      }
-      else if (whatsappNumber > whatsappNumber.slice(0, MAX_DIGITS)) {
-        return res.status(400).json({ success: false, message: strings.InvalidNumber});
-      }
-  
-      const otp = generateOTP();
-      const url = config.Api_Url
-      const apiUrl = `${url}/${whatsappNumber}?messageText=${otp}`;
-  
-      const response = await axios.post(
-        apiUrl,
-        {},
-        {
-          headers: {
-            Authorization: accessToken,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-  
-      res.json({ success: true, otp });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ success: false, error: error.message });
+    const MAX_DIGITS = 12;
+    const RESEND_COOLDOWN = 30000; // 30 seconds in milliseconds
+    let resendCooldownMap = new Map(); // Map to 
+
+    if (!whatsappNumber) {
+      return res.status(400).json({ success: false, message: strings.WhatsappNumberRequired });
     }
-  });
+    else if (whatsappNumber > whatsappNumber.slice(0, MAX_DIGITS)) {
+      return res.status(400).json({ success: false, message: strings.InvalidNumber });
+    }
+    const cooldownTimestamp = resendCooldownMap.get(whatsappNumber);
+    if (cooldownTimestamp && Date.now() - cooldownTimestamp < RESEND_COOLDOWN) {
+      const remainingCooldown = RESEND_COOLDOWN - (Date.now() - cooldownTimestamp);
+      return res.status(400).json({
+        success: false,
+        message: `Resend OTP cooldown active. Please wait ${remainingCooldown / 1000} seconds.`,
+      });
+    }
+
+    const otp = generateOTP();
+    const url = config.Api_Url
+    const apiUrl = `${url}/${whatsappNumber}?messageText=${otp}`;
+
+    const response = await axios.post(
+      apiUrl,
+      {},
+      {
+        headers: {
+          Authorization: accessToken,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    res.json({ success: true, otp });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+app.post('/resend_otp', async (req, res) => {
+  try {
+    const { whatsappNumber } = req.body;
+
+    const MAX_DIGITS = 12;
+    const RESEND_COOLDOWN = 30000; // 30 seconds in milliseconds
+    let resendCooldownMap = new Map(); // Map to 
+
+    if (!whatsappNumber) {
+      return res.status(400).json({ success: false, message: strings.WhatsappNumberRequired });
+    }
+    else if (whatsappNumber > whatsappNumber.slice(0, MAX_DIGITS)) {
+      return res.status(400).json({ success: false, message: strings.InvalidNumber });
+    }
+    const cooldownTimestamp = resendCooldownMap.get(whatsappNumber);
+    if (cooldownTimestamp && Date.now() - cooldownTimestamp < RESEND_COOLDOWN) {
+      const remainingCooldown = RESEND_COOLDOWN - (Date.now() - cooldownTimestamp);
+      return res.status(400).json({
+        success: false,
+        message: `Resend OTP cooldown active. Please wait ${remainingCooldown / 1000} seconds.`,
+      });
+    }
+
+    const otp = generateOTP();
+    const url = config.Api_Url
+    const apiUrl = `${url}/${whatsappNumber}?messageText=${otp}`;
+
+    const response = await axios.post(
+      apiUrl,
+      {},
+      {
+        headers: {
+          Authorization: accessToken,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    res.json({ success: true, otp });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
