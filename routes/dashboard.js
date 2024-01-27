fc464a4bea6cd668a0c8aa30473c9c2f1b2314f4
purushothbp <bpurushothamandasu@gmail.com>
2024-01-27 21:22:52 +0530

    update

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,6 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const config = require('../config.json');
-const { generateOTP } = require('../services'); // Make sure generateOTP is correctly implemented
 
 const app = express();
 const port = config.PORT;
